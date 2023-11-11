dba706f2708a80f5f258e54d902e1ee053e52cbc
Defen <defen64@gmail.com>
2023-11-12 00:07:47 +0600

    Live news

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -17,15 +17,11 @@ class _HomePageState extends State<HomePage> {
   String category = 'business';
   String newsChannel = 'bbc-news';
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Get.find<NewsController>().getNewsBycategory(category);
-      Get.find<NewsController>().getNewsBychannel(newsChannel);
-    });
+  Future<void> _refresh() async {
+    Get.find<NewsController>().getNewsBycategory(category);
+    Get.find<NewsController>().getNewsBychannel(newsChannel);
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,99 +32,128 @@ class _HomePageState extends State<HomePage> {
             'Daily Day',
             style: GoogleFonts.adamina(letterSpacing: .6, color: Colors.white),
           ),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+          actions: [
+            IconButton(
+              onPressed: () {
+                _refresh();
+              },
+              icon: const Icon(Icons.refresh),
+            )
+          ],
           elevation: 5,
         ),
         drawer: buildCustomDrawer(),
         body: Column(
           children: [
-          Expanded(
-            child: SizedBox(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: newsController.newsData.articles?.length ?? 0,
-              itemBuilder: (context, index) {
-                Articles article = newsController.newsData.articles![index];
+            Expanded(
+              child: GetBuilder<NewsController>(
+                builder: (newsController) {
+                  if(newsController.getNewsInprogess){
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }else {
+                    return SizedBox(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: newsController.newsData.articles?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        Articles article =
+                        newsController.newsData.articles![index];
 
-                return Container(
-                  margin: EdgeInsets.all(8.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Image
-                        Image.network(
-                          article.urlToImage ?? '',
-                          width: 150,
-                          height: 97,
-                          fit: BoxFit.cover,
-                        ),
+                        return Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Image
+                                Image.network(
+                                  article.urlToImage ?? '',
+                                  width: 150,
+                                  height: 97,
+                                  fit: BoxFit.cover,
+                                ),
 
-                        SizedBox(height: 8.0),
-                        // Subtitle
-                        SizedBox(
-                          width: 100,
-                          child: Text(
-                            article.title ?? '',
-                            style: TextStyle(fontSize: 10.0),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-        ),
-          ),
-          Expanded(
-            flex: 2,
-            child: SingleChildScrollView(
-              child: Container(
-                color: Colors.lightGreen,
-                height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: newsController.newsData.articles?.length ?? 0,
-                itemBuilder: (context, index) {
-                  Articles article = newsController.newsData.articles![index];
-
-                  return Container(
-                    margin: EdgeInsets.all(8.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Image
-                        Image.network(
-                          article.urlToImage ?? '',
-                          width: 150,
-                          height: 97,
-                          fit: BoxFit.cover,
-                        ),
-
-                        SizedBox(width: 8.0),
-                        // Subtitle
-                        Expanded(
-                          child: SizedBox(
-                            width: 100,
-                            child: Text(
-                              article.description ?? '',
-                              style: TextStyle(fontSize: 16.0),
+                                const SizedBox(height: 8.0),
+                                // Subtitle
+                                SizedBox(
+                                  width: 100,
+                                  child: Text(
+                                    article.title ?? '',
+                                    style: const TextStyle(fontSize: 10.0),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                      ],
+                        );
+                      },
                     ),
                   );
+                  }
                 },
               ),
-        ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: GetBuilder<NewsController>(
+                builder: (newsController) {
+                  if(newsController.getNewsInprogess){
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }else {
+                    return SingleChildScrollView(
+                    child: Container(
+                      color: Colors.lightGreen,
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: newsController.newsData.articles?.length ?? 0,
+                        itemBuilder: (context, index) {
+                          Articles article =
+                          newsController.newsData.articles![index];
+
+                          return Container(
+                            margin: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Image
+                                Image.network(
+                                  article.urlToImage ?? '',
+                                  width: 150,
+                                  height: 97,
+                                  fit: BoxFit.cover,
+                                ),
+
+                                const SizedBox(width: 8.0),
+                                // Subtitle
+                                Expanded(
+                                  child: SizedBox(
+                                    width: 100,
+                                    child: Text(
+                                      article.description ?? '',
+                                      style: const TextStyle(fontSize: 16.0),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -146,15 +171,15 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
             onTap: () {
               // Handle Home item tap
             },
           ),
           ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
             onTap: () {
               // Handle Settings item tap
             },
